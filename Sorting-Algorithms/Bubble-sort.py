e97e21af01918d4a4e2a212b7689fc5ef44ec114
yaswanth reddy <sivayaswanth59@gmail.com>
2025-08-14 22:05:49 +0530

    Bubble-sort

---
+++ Sorting-Algorithms/Bubble-sort.py
@@ -0,0 +1,20 @@
+# BubbleSort
+
+def bubbleSort(nums):
+    size = len(nums)
+  
+    for i in range(size-1):
+        # printing elements at Every pass
+        print(nums)
+
+        for j in range(size-1):
+          
+          if nums[j] > nums[j+1]: 
+              t = nums[j]
+              nums[j] = nums[j+1]
+              nums[j+1] = t
+            
+
+numbers = [5, 1, 4, 2, 8]
+bubbleSort(numbers)
+print(numbers)
