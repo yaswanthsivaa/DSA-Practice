221de0a7331fc2d6357a01b46827fd50d81593b2
yaswanth reddy <sivayaswanth59@gmail.com>
2026-01-27 23:02:26 +0530

    Next Greater Element solution using Stack
    Implement Next Greater Element algorithm using a stack.

---
+++ Stack/nge.py
@@ -0,0 +1,19 @@
+# Next Greater Element (Lc 503)
+     # Time Complexity = O(N)
+     # Space Complexity = O(N)
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        nge = [-1]*len(nums)
+
+        for i in range(2*len(nums)-1, -1, -1):
+            
+            while stack and stack[-1] <= nums[i % len(nums)]:
+                stack.pop()
+            
+            if i < len(nums):
+                nge[i] = stack[-1] if stack else -1
+            
+            stack.append(nums[i % len(nums)])
+        return nge
