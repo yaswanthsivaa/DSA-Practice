7686827966494ee0c41309e06486918b86b38a2a
yaswanth reddy <sivayaswanth59@gmail.com>
2026-02-10 21:41:34 +0530

    peak index in mountain array Problem solution using Binary Search
    Implement binary search to find peak index.

---
+++ BinarySearch/PeakIndexInaMountainArray.py
@@ -0,0 +1,33 @@
+# Peak Index in a Mountain Array (Leetcode 852)
+     # Time Complexity = O(log n)
+     # Space Complexity = O(1)
+
+lass Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+
+        # Linear Seach
+
+        # num = arr[0]
+        # for i in range(1, len(arr)-1):
+
+        #     if arr[i] > num and arr[i] > arr[i+1]:
+        #         return i
+        #     num = arr[i]
+
+
+        # Binary Search 
+
+        left = 0
+        right = len(arr) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid - 1] > arr[mid]:
+                right = mid
+            elif arr[mid + 1] > arr[mid]:
+                left = mid
+            
+            
