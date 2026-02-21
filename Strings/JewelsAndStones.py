266c94db8e62e1fd2aab38d90b28fb9ffa8cb40e
yaswanth reddy <sivayaswanth59@gmail.com>
2026-02-21 14:41:04 +0530

    Jewels and Stones problem (Leetcode 771)
    Implement solution for counting jewels in stones.

---
+++ Strings/JewelsAndStones.py
@@ -0,0 +1,12 @@
+# Jewels and Stones (Leetcode 771)
+    # Time Complexity = O(n)
+    # Space Complexity = O(1)
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewelsHave = 0
+        for i in range(len(stones)):
+            if stones[i] in jewels:
+                jewelsHave += 1
+        
+        return jewelsHave
