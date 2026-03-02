ef2e37e21893c04e913258517b5b573241404273
yaswanth reddy <sivayaswanth59@gmail.com>
2026-03-02 23:11:34 +0530

    finding the Maximum Avg SubArray's Average

--- SlidingWindowTechnique/MaximumAvgSubArray.py
+++ SlidingWindowTechnique/MaximumAvgSubArray.py
@@ -8,7 +8,7 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         
         windowSum = sum(nums[:k])
-
+ 
         maxSum = windowSum
 
         for i in range(k, len(nums)):
