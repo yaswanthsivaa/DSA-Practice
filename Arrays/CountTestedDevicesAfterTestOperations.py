cf252d4876ae26b6561374498395dfdc6789a6ba
yaswanth reddy <sivayaswanth59@gmail.com>
2026-03-13 23:03:51 +0530

    Count Tested Devices After Test Operations (Leetcode 2960)

---
+++ Arrays/CountTestedDevicesAfterTestOperations.py
@@ -0,0 +1,19 @@
+# Count Tested Devices After Test Operatoions (Leetcode 2960)
+    # Time Complexity = O(n^2)
+    # Space Complexity = O(1)
+
+
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        
+        n = len(batteryPercentages)
+        count = 0
+        for i in range(n):
+            if batteryPercentages[i] > 0:
+                count += 1
+                for j in range(i+1, n):
+                    batteryPercentages[j] = max(0,batteryPercentages[j]-1)
+
+        return count
+
+        
