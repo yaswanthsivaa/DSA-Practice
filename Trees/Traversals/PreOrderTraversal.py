7936f71801458fc3c248f005362182a0a30ac876
yaswanth reddy <sivayaswanth59@gmail.com>
2026-03-15 08:03:22 +0530

    Pre Order Traversal In Binary Tree (Leetcode 144)

---
+++ Trees/Traversals/PreOrderTraversal.py
@@ -0,0 +1,27 @@
+# Binary Tree Preorder Traversal (Leetcode 144)
+    # Time Complexity = O(N)
+    # Space Complexity = O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        result = []
+        def dfs(root):
+            
+            if not root:
+                return
+            
+            result.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+            
+        dfs(root)
+        
+        return result
+
