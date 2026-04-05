f3a65544095c8d071eb5d637956534d7d2416c17
yaswanth reddy <sivayaswanth59@gmail.com>
2026-04-05 17:50:40 +0530

    Diameter of Binary Tree solution (Leetcode 543)
    Implement diameter calculation for a binary tree.

---
+++ Trees/DFS/Diamerer.py
@@ -0,0 +1,28 @@
+# Diameter of Binary Tree (Leetcode 543)
+  # Time Complexity = O(N)
+  # Space Complexity = O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.diameter = 0
+        
+        def height(root):
+
+            if not root:
+                return 0
+            
+            left = height(root.left)
+            right = height(root.right)
+
+            self.diameter = max(self.diameter, left + right)
+            
+            return max(left, right) + 1
+        
+        height(root)
+        return self.diameter
