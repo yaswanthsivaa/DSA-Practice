436c47c425f61efebb83c4c847afb1f35f2d7b6d
yaswanth reddy <sivayaswanth59@gmail.com>
2026-04-06 15:28:23 +0530

    search in Binary Search Tree (Leetcode 700)

---
+++ Trees/DFS/BSI/searchInBST.py
@@ -0,0 +1,35 @@
+# Search in Binary Search Tree (Leetcode 700)
+
+# Time Complexity = O(N)
+# Space Complexity = O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        def search(node, val):
+            if not node:
+                return None
+            
+            if node.val == val:
+                return node
+            
+            elif val < node.val:
+                return search(node.left, val)
+            else:
+                return search(node.right, val)
+            
+        
+        return search(root, val)
+        
+
+        
+
+        
+
+        
