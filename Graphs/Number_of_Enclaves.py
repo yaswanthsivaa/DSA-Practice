61f89a958880568b44338b91f5ab6bd37b8a0afc
yaswanth reddy <sivayaswanth59@gmail.com>
2026-04-30 15:13:35 +0530

    Number_of_Enclaves (Leetcode 1020)
    Added comments for BFS and DFS approaches, including their time and space complexities.

--- Graphs/Number_of_Enclaves.py
+++ Graphs/Number_of_Enclaves.py
@@ -1,10 +1,13 @@
 # Number_of_Enclaves (Leetcode 1020)
-  # Time Complexity = O(M x N)
-  # Space Complexity = O(M x N)
 
 from collections import deque
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
+        # BFS
+      
+        # Time Complexity = O(M x N)
+        # Space Complexity = O(M x N)
+      
         rows = len(grid)
         cols = len(grid[0])
         Queue = deque()
@@ -35,6 +38,46 @@ class Solution:
                 
                 if grid[i][j] == "D":
                     grid[i][j] = 1
+        
+        return count
+
+        # Dfs 
+      
+        # Time Complexity = O(M x N)  : every cell is visited at most once
+        # Space Complexity = O(M x N) : recursion stack can go up to all cells in worst case
+      
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(i, j):
+
+            if 0 <= i < rows and 0 <= j < cols and grid[i][j] == 1:
+               grid[i][j] = "D"
+            else:
+                return
+
+            dfs(i+0, j+1)
+            dfs(i+0, j-1)
+            dfs(i-1, j+0)
+            dfs(i+1, j+0)
+
+        for i in range(rows):
+            for j in range(cols):
+
+                if (i == 0 or i == rows-1 or j == 0 or j == cols-1) and grid[i][j] == 1:
+                    dfs(i, j)
+        
+        
+        count = 0
+        for i in range(rows):
+            for j in range(cols):
+
+                if grid[i][j] == "D":
+                    grid[i][j] = 1
+                
+
+                elif grid[i][j] == 1:
+                    count += 1
         
         return count
 
