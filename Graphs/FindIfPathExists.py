0b479608fcd0bc72c7db4fc19744ecfe8a857ed3
yaswanth reddy <sivayaswanth59@gmail.com>
2026-05-02 15:45:49 +0530

    FindIfPathExists (Leetcode 1971)
    Implement DFS to check if a valid path exists between source and destination in a graph.

---
+++ Graphs/FindIfPathExists.py
@@ -0,0 +1,35 @@
+# FindIfPathExists (Leetcode 1971)
+  # Time Complexity = O(V + E)
+  # Space Complexity = O(V)
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+  
+        adj = {}
+
+        for i,j in edges:
+            if i not in adj:
+                adj[i] = []
+            
+            if j not in adj:
+                adj[j] = []
+            
+            adj[i].append(j)
+            adj[j].append(i)
+
+        vis = set()
+        def dfs(vertex):
+            
+            if vertex == destination:
+                return True
+            
+            vis.add(vertex)
+            for ch in adj[vertex]:
+                if ch not in vis:
+                   if dfs(ch):
+                      return True
+            
+            return False
+
+
+        return dfs(source)
