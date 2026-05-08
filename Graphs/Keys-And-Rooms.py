4b1940502eb721419a29f220b255267789d193f7
yaswanth reddy <sivayaswanth59@gmail.com>
2026-05-08 15:20:10 +0530

    Keys and Rooms (Leetcode 841)

---
+++ Graphs/Keys-And-Rooms.py
@@ -0,0 +1,29 @@
+# Keys and Rooms (Leetcode 841)
+  
+  # Breadth First Search
+  # Time Complexity = O(N + E)
+  # Space Complexity = O(N)
+
+from collections import deque
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        Queue = deque([0])
+        visited = set()
+
+        while Queue:
+            room = Queue.popleft()
+            if room not in visited:
+               visited.add(room)
+            
+               for i in rooms[room]:
+                   if i not in visited:
+                      Queue.append(i)
+        
+        return len(visited) == len(rooms)
+        
+
+            
+
+
